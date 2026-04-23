61f9b07c0624be25a1dec16a3f020620ea92f57e
mhazwan47-design <mhazwan47@gmail.com>
2026-04-23 16:33:09 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "/snitch-dashboard-v2/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: "docs",
-    emptyOutDir: false
-  }
+    emptyOutDir: false,
+  },
 });
